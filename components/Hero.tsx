874b56fea10171d81fd84c3caa6a280d92850c98
ASproson <a.sproson@outlook.com>
2024-06-09 15:43:22 +0100

    Name tag

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerate";
 
 const Hero = () => {
   return (
@@ -25,6 +26,13 @@ const Hero = () => {
           <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
             Dynamic
           </h2>
+          <TextGenerateEffect
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+            words="Transforming Concepts into Seamless Experiences"
+          />
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi, I&apos;m Anthony, a full stack developer
+          </p>
         </div>
       </div>
     </div>
